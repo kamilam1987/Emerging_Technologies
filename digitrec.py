2d7ff75ed6080c9ebab606f51d5986a68ef9c92c
kamilam1987 <kamilam1987@gmail.com>
2018-11-29 16:23:49 +0000

    Added script that creates th model,train and split MNIST data

--- digitrec.py
+++ digitrec.py
@@ -1,37 +1,79 @@
-# Logistic Regression MNIST
-# Source code adapted from: https://www.codementor.io/mgalarny/making-your-first-machine-learning-classifier-in-scikit-learn-python-db7d7iqdh
+# Handwritten Digit Recognition using Deep Learning, Keras and Python
+# Source code adapted from: https://gogul09.github.io/software/digits-recognition-mlp
 
-# Import MNIST from sklearn.datasets
-from sklearn.datasets import fetch_mldata
-# Import train_test_split for traning and spliting MNIST data
-from sklearn.model_selection import train_test_split
+# Imports
 # Import numpy
 import numpy as np
-# Import matplotlib to show an images
+# Import matplotlib to show plots
 import matplotlib.pyplot as plt
+# Import Sequential model which is a linear stack of layers
+from keras.models import Sequential
+#Keras Imports to create the neural network model with neurons, layers and other utilities.
+from keras.layers.core import Dense, Activation, Dropout
+# Import MNIST datase
+from keras.datasets import mnist
+from keras.utils import np_utils
 
-# Load the MNIST data
-mnist = fetch_mldata('MNIST original')
+# Fixes a random seed for reproducibility
+np.random.seed(9)
 
-# There are 70,000 images (28 by 28 images for a dimensionality of 784)
-# Shows the amount of images in MNIST dataset
-print(mnist.data.shape)
+# User inputs
+nb_epoch = 25 # Number of iterations needed for the network to minimize the loss function, so that it learns the weights.
+num_classes = 10 # Total number of class labels or classes involved in the classification problem.
+batch_size = 128 # Number of images given to the model at a particular instance.
+train_size = 60000 # Number of training images to train the model.
+test_size = 10000 # Number of testing images to test the model.
+v_length = 784 # imension of flattened input image size i.e. if input image size is [28x28], then v_length = 784.
 
-# There are 70,000 labels 
-# Shows the amount of labels in MNIST dataset
-print(mnist.target.shape)
+# Splits the MNIST data into train and test
+(trainData, trainLabels), (testData, testLabels) = mnist.load_data()
+print ("[INFO] train data shape: {}".format(trainData.shape))
+print ("[INFO] test data shape: {}".format(testData.shape))
+print( "[INFO] train samples: {}".format(trainData.shape[0]))
+print ("[INFO] test samples: {}".format(testData.shape[0]))
 
-# Splits the data into training and test data sets the test_size=1/7.0, training set size 60,000 images and the test set size of 10,000
-train_img, test_img, train_lbl, test_lbl = train_test_split(mnist.data, mnist.target, test_size=1/7.0, random_state=0)
+# Reshapes the dataset
+trainData = trainData.reshape(train_size, v_length)
+testData = testData.reshape(test_size, v_length)
+trainData = trainData.astype("float32")
+testData = testData.astype("float32")
+trainData /= 255
+testData /= 255
 
-# Determine figure size
-plt.figure(figsize=(20,4))
+print ("[INFO] train data shape: {}".format(trainData.shape))
+print ("[INFO] test data shape: {}".format(testData.shape))
+print ("[INFO] train samples: {}".format(trainData.shape[0]))
+print ("[INFO] test samples: {}".format(testData.shape[0]))
 
-# Loop through images and labels, shows the first five train images and the first five train labels
-for index, (image, label) in enumerate(zip(train_img[0:5], train_lbl[0:5])):
-    plt.subplot(1, 5, index + 1)
-    plt.imshow(np.reshape(image, (28,28)), cmap='gray')
-    plt.title('Training: %i\n' % label, fontsize = 20)
+# convert class vectors to binary class matrices --> one-hot encoding
+mTrainLabels = np_utils.to_categorical(trainLabels, num_classes)
+mTestLabels = np_utils.to_categorical(testLabels, num_classes)
 
-    # Shows plots
-    plt.show()
+# Creates the model
+model = Sequential()
+# Two hidden layers are used with 512 neurons in hidden layer 1 a
+model.add(Dense(512, input_shape=(784,)))
+model.add(Activation("relu"))# Activation function for hidden layers
+model.add(Dropout(0.2))
+model.add(Dense(256))#  256 neurons in hidden layer 2
+model.add(Activation("relu"))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes))
+model.add(Activation("softmax"))#  Activation function for output layer.
+
+# Summarizes the model
+model.summary()
+
+# Compiles the model
+# categorical_crossentropy as the loss function, adam as the optimizer and accuracy as our performance metric.
+model.compile(loss="categorical_crossentropy",optimizer="adam",metrics=["accuracy"])
+
+# Fits the model
+# Takes two arguments : trainin data and training labels
+history = model.fit(trainData, mTrainLabels,validation_data=(testData, mTestLabels),batch_size=batch_size,nb_epoch=nb_epoch,verbose=2)
+
+# Prints the history keys
+print (history.history.keys())
+
+# Evaluate the model and makes prediction
+scores = model.evaluate(testData, mTestLabels, verbose=0)
